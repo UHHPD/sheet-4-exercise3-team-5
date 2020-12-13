55941332f2824862d70fc105d3556ab05bde004f
MiHuf <michael@hufschmidt-web.de>
2020-12-13 12:09:33 +0100

    fixed indentation

--- Data.hh
+++ Data.hh
@@ -5,21 +5,21 @@
 #include <string>
 
 class Data {
- public:
-  Data(const std::string& filename);
+  public:
+    Data(const std::string& filename);
 
-  unsigned int size() const { return m_data.size(); }
-  double measurement(int i) const { return m_data[i]; }
-  double binCenter(int i) const { return 0; }
-  double binLow(int i) const { return 0; }
-  double binHigh(int i) const { return 0; }
-  double error(int i) const { return 0; }
+    unsigned int size() const { return m_data.size(); }
+    double measurement(int i) const { return m_data[i]; }
+    double binCenter(int i) const { return 0; }
+    double binLow(int i) const { return 0; }
+    double binHigh(int i) const { return 0; }
+    double error(int i) const { return 0; }
 
- private:
-  Data() {}  // disallow empty data set
-  void assertSizes();
-  std::vector<double> m_data;
-  std::vector<double> m_bins;
+  private:
+    Data() {}  // disallow empty data set
+    void assertSizes();
+    std::vector<double> m_data;
+    std::vector<double> m_bins;
 };
 
 #endif
